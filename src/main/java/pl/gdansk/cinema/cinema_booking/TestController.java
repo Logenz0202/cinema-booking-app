edb5e367b964d45d296dafaf77d62ad0c7564c0c
Szymon Ligenza <szymek.ligenza@gmail.com>
2026-01-17 19:46:13 +0100

    project init

---
+++ src/main/java/pl/gdansk/cinema/cinema_booking/TestController.java
@@ -0,0 +1,13 @@
+package pl.gdansk.cinema.cinema_booking;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Projekt działa.";
+    }
+}
---
+++ src/test/java/pl/gdansk/cinema/cinema_booking/CinemaBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gdansk.cinema.cinema_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
